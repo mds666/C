01c08eda2554a1debbc1e69fd750e7f88464e623
莫大帅 <2960384857@qq.com>
2022-02-12 20:53:30 +0800

    2022-2-12

---
+++ 2022-2-12/源.c
@@ -0,0 +1,44 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//
+//	//int a = 10;//c++注释风格
+//	/*
+//	int b = 10;c语言的注释风格-不支持嵌套注释
+//	*/
+//
+//
+//	return 0;
+//}
+
+//选择语句
+//int main()
+//{
+//	int input = 0;
+//	printf("好好学习吗？（1/0）？");
+//	scanf("%d", &input);
+//	if (input = 1)
+//		printf("好offer\n");
+//	else
+//		printf("卖红薯\n");
+//
+//	return 0;
+//}
+
+//循环语句
+//int main()
+//{
+//	int line = 0;
+//	while (line < 30000)
+//	{
+//		printf("写代码:%d\n",line);
+//		line++;
+//	}
+//	if (line = 30000);
+//	printf("好offer\n");
+//
+//	return 0;
+//}
+
+
